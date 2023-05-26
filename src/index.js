502ae507043749207ab37bb94b48d1cde52244af
M1seryy <62029606+M1seryy@users.noreply.github.com>
2023-05-26 14:26:10 +0300

    .

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-// Описаний в документації
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 // Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
@@ -7,7 +7,6 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 const list = document.querySelector('.gallery');
 const form = document.querySelector('.search-form');
 const btn = document.querySelector('.load-more');
-let gallery = new SimpleLightbox('.gallery a');
 let page = 1;
 function formHandler(e) {
   e.preventDefault();
@@ -15,18 +14,21 @@ function formHandler(e) {
   const value = form.searchQuery.value;
   page = 1;
   serviceImage(value.trim(), page);
-  btn.hidden = false;
 }
 form.addEventListener('submit', formHandler);
 async function serviceImage(name, nextPage) {
   const URL = `https://pixabay.com/api/?key=36745882-9a469cd98fdea02c7a63719ef&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${nextPage}&per_page=40`;
   const response = await axios.get(URL).then(res => {
     if (res.data.totalHits === 0) {
-      console.log("We're sorry, but you've reached the end of search results.");
+      Notify.failure(
+        "We're sorry, but you've reached the end of search results."
+      );
+      return;
+    } else {
+      btn.hidden = false;
+      markUp(res.data);
+      let lightbox = new SimpleLightbox('.gallery a');
     }
-    console.log(res.data);
-    markUp(res.data);
-    var lightbox = new SimpleLightbox('.gallery a');
   });
 }
 
