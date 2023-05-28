88d31212720b7d9b39fc0eb4fd62ecaa559288e7
M1seryy <62029606+M1seryy@users.noreply.github.com>
2023-05-28 16:27:42 +0300

    rework

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 const list = document.querySelector('.gallery');
 const form = document.querySelector('.search-form');
 const btn = document.querySelector('.load-more');
+let lightbox = new SimpleLightbox('.gallery a');
 let page = 1;
 function formHandler(e) {
   e.preventDefault();
@@ -20,15 +21,24 @@ async function serviceImage(name, nextPage) {
   const URL = `https://pixabay.com/api/?key=36745882-9a469cd98fdea02c7a63719ef&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${nextPage}&per_page=40`;
   try {
     const response = await axios.get(URL).then(res => {
+      console.log(res.data);
       if (res.data.totalHits === 0) {
         Notify.failure(
-          "We're sorry, but you've reached the end of search results."
+          "We're sorry, there is no images."
         );
         return;
+      }
+      if (res.data.hits.length < 40) {
+        Notify.info(
+          "We're sorry, but you've reached the end of search results."
+        );
+        btn.hidden = true;
+        markUp(res.data);
+        lightbox.refresh();
       } else {
         btn.hidden = false;
         markUp(res.data);
-        let lightbox = new SimpleLightbox('.gallery a');
+        lightbox.refresh();
       }
     });
   } catch (err) {
@@ -78,5 +88,6 @@ function markUp(data) {
 function nextPegeService() {
   page = page + 1;
   serviceImage(form.searchQuery.value.trim(), page);
+  lightbox.refresh();
 }
 btn.addEventListener('click', nextPegeService);
